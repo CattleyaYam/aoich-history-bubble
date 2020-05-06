df20584679d729ce00500beadcd03e0e9514e515
yamashitayohei <yamanobu.yamashita@gmail.com>
2020-05-06 15:37:39 +0900

    sixteenth commit

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-let camera, scene, renderer;
-let controls;
-let bubbles = [];
-let clock;
+const container = document.getElementById( 'canvas-container' );
 
 const circleCenterPos = new THREE.Vector2(2600,6700);
 const circleRadius = 1100;
@@ -13,44 +10,114 @@ const d0 = r*2;
 const d1 = d0 * 1.4;
 const offset = 100000000000000;
 
+let camera, scene, renderer;
+let controls;
+let bubbles = [];
+let clock;
+
 init();
 animate();
 onWindowResize();
  
 function init() {
-    //Init threejs
-    camera = new THREE.PerspectiveCamera( 45, document.documentElement.clientWidth / document.documentElement.clientHeight, 1, 45000 );
-    camera.position.z = 35000;
+    //container size
+    const w = container.getBoundingClientRect().width;
+    const h = container.getBoundingClientRect().height;
+
+    //camera
+    camera = new THREE.PerspectiveCamera( 45, w/h, 1, 1000 );
+    camera.position.z = 180;
+    
+    //scene
     scene = new THREE.Scene();
-    clock = new THREE.Clock();
+    scene.background = new THREE.Color( 0xFFFFFF );
+
+    //renderer
     renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight);
-    document.body.appendChild( renderer.domElement );
+    renderer.setSize( w, h);
+    container.appendChild( renderer.domElement );
+
+    //clock
+    clock = new THREE.Clock();
+    
+    //control
     controls = new THREE.TrackballControls( camera, renderer.domElement );
     controls.minDistance = 1;
-    controls.maxDistance = 45000;
+    controls.maxDistance = 1000;
     controls.noRotate = true;
-    controls.addEventListener( 'change', animate );
+    controls.addEventListener( 'change', render );
     window.addEventListener( 'resize', onWindowResize, false );
 
-    //Load Background-image
-    let texture = new THREE.TextureLoader().load("images/aoi.png");
-    let material = new THREE.MeshBasicMaterial( { map: texture, opacity: 0.8, transparent: true } );
-    let geometry = new THREE.PlaneBufferGeometry();
-    let mesh = new THREE.Mesh( geometry, material );
+    //Aoi image
+    const texture = new THREE.TextureLoader().load("images/aoi.png");
+    texture.wrapS = texture.wrapT = THREE.ClampToEdgeWrapping;
+    texture.minFilter = THREE.LinearFilter;
+    const material = new THREE.MeshBasicMaterial( { map: texture, opacity: 1.0, transparent: true } );
+    const geometry = new THREE.PlaneBufferGeometry(85,120);
+    const mesh = new THREE.Mesh( geometry, material );
     scene.add( mesh );
+
+    //Aoi history
+    const req = new XMLHttpRequest();
+    req.open("get", "data/aoi_movie.csv", true);
+    req.send(null);
+    req.onload = function(){
+        let result = [];
+        let tmp = req.responseText.split("\n");
+        let playId = "";
+        for(let i=0;i<1;++i){
+            result[i] = tmp[i].split(',');
+            playId = result[i][1].substr(-11);
+            let tmp2 = "http://img.youtube.com/vi/"+playId+"/mqdefault.jpg";
+            console.log(tmp2);
+        }
+    }
 }
+
+function forceDownload(blob, filename) {
+    var a = document.createElement('a');
+    a.download = filename;
+    a.href = blob;
+    // For Firefox https://stackoverflow.com/a/32226068
+    document.body.appendChild(a);
+    a.click();
+    a.remove();
+  }
+  
+  // Current blob size limit is around 500MB for browsers
+  function downloadResource(url, filename) {
+    if (!filename) filename = url.split('\\').pop().split('/').pop();
+    fetch(url, {
+        headers: new Headers({
+          'Origin': location.origin
+        }),
+        mode: 'cors'
+      })
+      .then(response => response.blob())
+      .then(blob => {
+        let blobUrl = window.URL.createObjectURL(blob);
+        forceDownload(blobUrl, filename);
+      })
+      .catch(e => console.error(e));
+  }
+  downloadResource("http://img.youtube.com/vi/Nk-9BlTz1mA/mqdefault.jpg");
  
 function animate() {
     requestAnimationFrame( animate );
     controls.update();
+    render();
+}
+
+function render() {
     renderer.render( scene, camera );
 }
 
 function onWindowResize() {
-    camera.aspect = document.documentElement.clientWidth / document.documentElement.clientHeight;
+    const w = container.getBoundingClientRect().width;
+    const h = container.getBoundingClientRect().height;
+    camera.aspect = w/h;
     camera.updateProjectionMatrix();
-    renderer.setSize( document.documentElement.clientWidth, document.documentElement.clientHeight );
+    renderer.setSize(w,h);
     render();
 }
 
